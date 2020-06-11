4a4251d4ec8d9925cd1e4db27b05440720a451cc
Devarshi <devarshirawal@outlook.com>
2020-06-11 00:52:48 -0700

    Add functions for reading and transforming data

--- cap_package/SpotipyCollectPub.py
+++ cap_package/SpotipyCollectPub.py
@@ -1,10 +1,13 @@
+from cap_package import SpotipyCollect as sc
 import demoji
+import numpy as np
 import pandas as pd
 from pandas import json_normalize
+import random
 import re
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from cap_package import SpotipyCollect as sc
+
 
 demoji.download_codes()
 
@@ -286,3 +289,141 @@ def user_plid_pair(user_ids, playlists):
     user_plid = [(u[0], x[0]) for u in paired for x in u[1]]
 
     return user_plid
+
+
+# --------------------------------------------------------------------------------
+#  Functions for reading and transforming dataset
+# --------------------------------------------------------------------------------
+
+def read_dataset(path_, segments=True, tempo=False):
+    '''
+    Read analysis(dataframes) dataset stored as parquet files.
+
+    path_ : path to dataset directory
+    segments : Boolean - True to read segments files
+    tempo : Boolean - True to read tempo files. Default False
+    return : a dict with key/value pairs for all playlists in the folder.
+             Key : Name of the playlist (string)
+             Value : a dict of track analysis dataframes of all tracks from the playlist
+    '''
+    dataset = []
+    for pl in path_.iterdir():
+
+        pl_info = [pl.name]
+
+        if tempo:
+            tempo_list = [(re.sub('_tempo.parquet', '', t.name), pd.read_parquet(t)) for t in pl.glob('*_tempo.parquet')]
+            pl_info.append(tempo_list)
+
+        if segments:
+            segments_list = [(re.sub('_segments.parquet', '', s.name), pd.read_parquet(s)) for s in pl.glob('*_segments.parquet')]
+            pl_info.append(segments_list)
+
+        dataset.append(pl_info)
+
+    return dataset
+
+
+def split_columns(df, pitch_cols, timbre_cols):
+    '''
+    df : dataframe of track segments with columns of pitch vector and timbre vector
+    pitch_cols : List of pitch column names to split in
+    timbre_cols : List of timbre column names to split in
+
+    return : Updated dataframe with only pitches and timbre elements columns
+    '''
+
+    new_seg = pd.DataFrame()
+    new_seg[pitch_cols] = pd.DataFrame(df.pitches.tolist(), index=df.index)
+    new_seg[timbre_cols] = pd.DataFrame(df.timbre.tolist(), index=df.index)
+
+    return new_seg
+
+
+def timbre_minmax_tr(track_seg):
+    '''
+    Get the min and max of timbre values for a track
+
+    track_segs : dataframe of track segments containing columns - timbre_1 to timbre_12
+    return : min and max values of timbre elements as lists
+    '''
+
+    timbre_ = ['timbre_{}'.format(i + 1) for i in range(12)]
+    timbre_min = [track_seg[timb].min() for timb in timbre_]
+    timbre_max = [track_seg[timb].max() for timb in timbre_]
+
+    return timbre_min, timbre_max
+
+
+def pop_timbre_minmax(timbre_mins, timbre_maxs):
+    '''
+    Evaluates population minimum and maximum values of all timbre elements.
+
+    timbre_mins : list of minimum values of timbre elements, for all tracks
+    timbre_maxs : list of maximum values of timbre elements, for all tracks
+
+    return: minimum and maximum values of timbre elements over the whole dataset
+    '''
+    timbre_ = ['timbre_{}'.format(i + 1) for i in range(12)]
+    pop_timbre_min = []
+    pop_timbre_max = []
+    for i in range(len(timbre_)):
+        pop_timbre_min.append(np.array([t[i] for t in timbre_mins]).min())
+        pop_timbre_max.append(np.array([t[i] for t in timbre_maxs]).max())
+
+    return pop_timbre_min, pop_timbre_max
+
+
+def minmax_scale(x, mins, maxs, a=-1, b=1):
+    '''
+    Scale vector x between -1 and 1.
+    x : vector/ array to be scaled
+    mins : vector/ array of min values
+    maxs : vector/ array of max values
+    a : lower bound of scaling. Default -1
+    b : upper bound of scaling. Default 1
+    '''
+    a_ = a * (np.ones(len(x)))
+    b_ = b * (np.ones(len(x)))
+    rescaled_x = a_ + ((x - mins) * (b_ - a_)) / (maxs - mins)
+
+    return rescaled_x
+
+
+def transform_dataset(dataset, timbre_min, timbre_max, num_seg=50):
+    '''
+    Create input arrays to be fed into a model with label provided in tuples).
+
+    dataset : list of track segmentsdataframes
+    timbre_min : List or numpy array of minimums of timbre values over the whole dataset
+    timbre_max : List or numpy array of maximums of timbre values over the whole dataset
+    num_seg : Default : 50 - Number of segments to be taken for input.
+
+    returns : data - list of tuples of input data arrays and (one-hot)encoded labels.
+              categories - array  of unique playlist name/labels in the data.
+              num_tracks -  number of tracks in each category/playlist
+              Currently only using segment arrays consisting of sequences pitch array and timbre values flattened for input.
+              Future edit should account for other features, i.e tempo and/or audio features from spotify.
+    '''
+
+    timbre_ = ['timbre_{}'.format(i + 1) for i in range(12)]
+
+    timbre_min = np.array(timbre_min)
+    timbre_max = np.array(timbre_max)
+
+    data_arrays = []
+    for df in dataset:
+
+        # Scale timbre values
+        df[timbre_] = df[timbre_].apply(minmax_scale, args=(timbre_min, timbre_max), axis=1)
+
+        # filter index of n -> num_seg randomly chosen segments
+        indx = random.sample(list(df.index), num_seg)
+        df_ = df.loc[indx]
+
+        # Convert dataframe to numpy array
+        segments = df_.to_numpy()
+        segments = np.hstack(segments)
+        data_arrays.append(segments)
+
+    return data_arrays
